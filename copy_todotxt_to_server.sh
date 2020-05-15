3c94fd392dee4e79d0aa92db7989dd223f2775be
Sean Breckenridge <seanbrecke@gmail.com>
2020-05-14 17:40:54 -0700

    add a timeout on the scp

--- copy_todotxt_to_server.sh
+++ copy_todotxt_to_server.sh
@@ -3,5 +3,5 @@
 TODO_FILE="${HOME}/.config/todo/todo.txt"
 SSH_KEYFILE="${HOME}/.ssh/vultr"
 
-find "$TODO_FILE" | entr scp -v -o IPQoS=0 -i "$SSH_KEYFILE" "$TODO_FILE" "$USER"@140.82.50.43:.todo/todo.txt
+find "$TODO_FILE" | entr scp -v -o IPQoS=0 -o ConnectTimeout=60 -i "$SSH_KEYFILE" "$TODO_FILE" "$USER"@140.82.50.43:.todo/todo.txt
 
